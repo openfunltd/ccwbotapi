58436073a79ccf2382893f8f1931f6d2fc49de0e
Ronny Wang <ronnywang@gmail.com>
2024-12-16 13:50:53 +0800

    add google login config

--- config.sample.inc.php
+++ config.sample.inc.php
@@ -4,3 +4,6 @@ putenv('APP_NAME=Mini Engine sample application');
 putenv('DATABASE_URL=pgsql://user:password@localhost:5432/dbname');
 putenv('SESSION_SECRET=___SECRET___');
 putenv('SESSION_DOMAIN='); // optional
+// Google OAuth2: https://console.cloud.google.com/apis/credentials
+putenv('GOOGLE_CLIENT_ID=99999999999-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx.apps.googleusercontent.com');
+putenv('GOOGLE_CLIENT_SECRET=GGGGGG-xxxxxxxxxxxxxxxxxxxxxxxxxxxx');
