fb980302d9924956603499ba436e3977f2a4d55f
Ronny Wang <ronnywang@gmail.com>
2025-01-02 15:55:46 +0800

    add default config

--- init.inc.php
+++ init.inc.php
@@ -5,6 +5,10 @@ define('MINI_ENGINE_ROOT', __DIR__);
 require_once(__DIR__ . '/mini-engine.php');
 if (file_exists(__DIR__ . '/config.inc.php')) {
     include(__DIR__ . '/config.inc.php');
+} elseif (file_exists("/srv/config/ccwbotapi.inc.php")) {
+    include("/srv/config/ccwbotapi.inc.php");
+} else {
+    die("config.inc.php not found");
 }
 set_include_path(
     __DIR__ . '/libraries'
