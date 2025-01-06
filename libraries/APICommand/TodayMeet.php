b351f15b42ae50c1e9fd42f3eed3000f8045dabe
Ronny Wang <ronnywang@gmail.com>
2025-01-06 15:36:58 +0800

    add TodayMeet API

---
+++ libraries/APICommand/TodayMeet.php
@@ -0,0 +1,33 @@
+<?php
+
+class APICommand_TodayMeet extends APICommand
+{
+    public static $name = '列出今日會議';
+    public static $description = '列出今日或是指定日期的會議';
+    public static $params = [
+        '日期',
+    ];
+
+    public static function run($params, $user = null)
+    {
+        if (!$params[0]) {
+            $params[0] = date('Y-m-d');
+        } else {
+            $params[0] = date('Y-m-d', strtotime($params[0]));
+        }
+        $ret = LYAPI::apiQuery("/meets/?日期={$params[0]}", "查詢 {$params[0]} 會議資訊");
+        if ($ret->error) {
+            return [
+                'type' => 'text',
+                'data' => [
+                    'text' => "查無此會議",
+                ],
+            ];
+        
+        }
+        return [
+            'type' => 'meet',
+            'data' => array_map(['DataBuilder', 'buildMeet'], $ret->meets),
+        ];
+    }
+}
